aca8ce0c644ec556bd6bc3bccc7ae15c5fafded9
OlehVahurak <149818828+OlehVahurak@users.noreply.github.com>
2025-11-08 17:01:13 +0100

    Remove unanswered flag from fact_questions.sql
    Remove the unanswered flag from the SQL query.

--- models/marts/facts/fact_questions.sql
+++ models/marts/facts/fact_questions.sql
@@ -13,7 +13,6 @@ SELECT
     q.comment_count,
     -- Użyj CAST, jak to zrobiłeś, do tworzenia flagi INT64
     CAST((q.accepted_answer_id IS NOT NULL) AS INT64) AS is_answered,
-    CAST((q.accepted_answer_id IS NULL) AS INT64) AS is_unanswered_flag -- Dodaj flagę dla niewykonanych (unanswered)
 FROM {{ source('stackoverflow_raw', 'posts_questions') }} q
 LEFT JOIN {{ ref('dim_time') }} dt -- Używamy ref() do wymiaru czasu
     ON DATE(q.creation_date) = dt.full_date
