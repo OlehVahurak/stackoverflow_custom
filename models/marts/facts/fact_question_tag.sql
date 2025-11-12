9a1af716fdc92a338f0b7db1ffcf737eacd746d4
OlehVahurak <149818828+OlehVahurak@users.noreply.github.com>
2025-11-12 15:36:45 +0100

    Fix source reference in fact_question_tag.sql

--- models/marts/facts/fact_question_tag.sql
+++ models/marts/facts/fact_question_tag.sql
@@ -5,9 +5,9 @@
 SELECT
     q.id AS question_id,
     t.tag_id
-FROM {{ source('stackoverflow_raw', 'posts_questions') }} q,
+FROM {{ source('stackoverflow_raw_data', 'posts_questions') }} q,  -- <-- tu zmiana
 UNNEST(SPLIT(q.tags, ';')) AS single_tag
-JOIN {{ ref('dim_tags') }} t -- Używamy ref() do wymiaru tagów
+JOIN {{ ref('dim_tags') }} t
     ON single_tag = t.tag_name
 WHERE q.post_type_id = 1
-AND q.tags IS NOT NULL
+  AND q.tags IS NOT NULL
